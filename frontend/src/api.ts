b8028ee905825deb32440fb94f80a0c6b72b1b5f
ikanago <ikanago-dev@protonmail.com>
2023-09-05 21:29:46 +0900

    Farewell to prettier and eslint: Welcome, biome!

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -6,125 +6,125 @@ import { type paths } from "./schema";
 const api = "http://localhost:3000/api";
 
 type ResponseOperation<Op> = Op extends { responses: infer Statuses }
-    ? {
-          [K in keyof Statuses]: Statuses[K] extends { content: infer Content }
-              ? Content extends { "application/json": infer Data }
-                  ? { status: K; data: Data }
-                  : { status: K }
-              : never;
-      }[keyof Statuses]
-    : never;
+  ? {
+      [K in keyof Statuses]: Statuses[K] extends { content: infer Content }
+        ? Content extends { "application/json": infer Data }
+          ? { status: K; data: Data }
+          : { status: K }
+        : never;
+    }[keyof Statuses]
+  : never;
 
 export type ResponseGet<P extends keyof paths> = paths[P] extends {
-    get: infer Get;
+  get: infer Get;
 }
-    ? ResponseOperation<Get>
-    : never;
+  ? ResponseOperation<Get>
+  : never;
 
 export type ResponsePost<P extends keyof paths> = paths[P] extends {
-    post: infer Post;
+  post: infer Post;
 }
-    ? ResponseOperation<Post>
-    : never;
+  ? ResponseOperation<Post>
+  : never;
 
 export type ResponseDelete<P extends keyof paths> = paths[P] extends {
-    delete: infer Delete;
+  delete: infer Delete;
 }
-    ? ResponseOperation<Delete>
-    : never;
+  ? ResponseOperation<Delete>
+  : never;
 
 type Parameters<P extends keyof paths> = paths[P] extends { get: infer Get }
-    ? Get extends { parameters: infer Params }
-        ? Params extends { path: infer Path }
-            ? Path
-            : never
-        : never
-    : never;
+  ? Get extends { parameters: infer Params }
+    ? Params extends { path: infer Path }
+      ? Path
+      : never
+    : never
+  : never;
 
 type Payload<P extends keyof paths> = paths[P] extends { post: infer Post }
-    ? Post extends {
-          requestBody: {
-              content: {
-                  "application/json": infer Body;
-              };
-          };
-      }
-        ? Body
-        : undefined
-    : undefined;
+  ? Post extends {
+      requestBody: {
+        content: {
+          "application/json": infer Body;
+        };
+      };
+    }
+    ? Body
+    : undefined
+  : undefined;
 
 const apiGet =
-    <P extends keyof paths>(path: P) =>
-    async (params?: Parameters<P>): Promise<ResponseGet<P>> => {
-        let replacedPath;
-        if (params !== undefined) {
-            replacedPath = path.replace(/{([^}]*)}/g, (_, key) => params[key]);
-        } else {
-            replacedPath = path;
-        }
-        const res = await fetch(`${api}${replacedPath}`, {
-            method: "GET",
-            credentials: "include",
-        });
+  <P extends keyof paths>(path: P) =>
+  async (params?: Parameters<P>): Promise<ResponseGet<P>> => {
+    let replacedPath;
+    if (params !== undefined) {
+      replacedPath = path.replace(/{([^}]*)}/g, (_, key) => params[key]);
+    } else {
+      replacedPath = path;
+    }
+    const res = await fetch(`${api}${replacedPath}`, {
+      method: "GET",
+      credentials: "include",
+    });
 
-        const data = await res.json();
-        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-        const response = {
-            status: res.status,
-            data,
-        } as ResponseGet<P>;
-        return response;
-    };
+    const data = await res.json();
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const response = {
+      status: res.status,
+      data,
+    } as ResponseGet<P>;
+    return response;
+  };
 
 const apiPost =
-    <P extends keyof paths>(path: P) =>
-    async (payload: Payload<P>): Promise<ResponsePost<P>> => {
-        const res = await fetch(`${api}${path}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(payload),
-        });
+  <P extends keyof paths>(path: P) =>
+  async (payload: Payload<P>): Promise<ResponsePost<P>> => {
+    const res = await fetch(`${api}${path}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
 
-        if (res.status === 204) {
-            // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-            return {
-                status: res.status,
-            } as ResponsePost<P>;
-        }
+    if (res.status === 204) {
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      return {
+        status: res.status,
+      } as ResponsePost<P>;
+    }
 
-        const data = await res.json();
-        return {
-            status: res.status,
-            data,
-        } as unknown as ResponsePost<P>;
-    };
+    const data = await res.json();
+    return {
+      status: res.status,
+      data,
+    } as unknown as ResponsePost<P>;
+  };
 
 const apiDelete =
-    <P extends keyof paths>(path: P) =>
-    async (payload: Payload<P>): Promise<ResponseDelete<P>> => {
-        const res = await fetch(`${api}${path}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(payload),
-        });
+  <P extends keyof paths>(path: P) =>
+  async (payload: Payload<P>): Promise<ResponseDelete<P>> => {
+    const res = await fetch(`${api}${path}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
 
-        if (res.status === 204) {
-            // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-            return {
-                status: res.status,
-            } as ResponseDelete<P>;
-        }
+    if (res.status === 204) {
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      return {
+        status: res.status,
+      } as ResponseDelete<P>;
+    }
 
-        const data = await res.json();
-        return {
-            status: res.status,
-            data,
-        } as unknown as ResponseDelete<P>;
-    };
+    const data = await res.json();
+    return {
+      status: res.status,
+      data,
+    } as unknown as ResponseDelete<P>;
+  };
 
 export const me = apiGet("/me");
 
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,65 +1,61 @@
-import { useMe, useMyPosts } from "../hooks";
 import { useState } from "react";
 import { createPost } from "../api";
+import { useMe, useMyPosts } from "../hooks";
 
 const Home = () => {
-    const [content, setContent] = useState("");
-    const [error, setError] = useState("");
-    const me = useMe();
-    const { res, mutate } = useMyPosts();
+  const [content, setContent] = useState("");
+  const [error, setError] = useState("");
+  const me = useMe();
+  const { res, mutate } = useMyPosts();
 
-    const submit = async () => {
-        const res = await createPost({ content });
-        if (res.status === 204) {
-            setContent("");
-            await mutate();
-        } else {
-            setError(res.data.error);
-        }
-    };
+  const submit = async () => {
+    const res = await createPost({ content });
+    if (res.status === 204) {
+      setContent("");
+      await mutate();
+    } else {
+      setError(res.data.error);
+    }
+  };
 
-    return (
-        <>
-            {me?.status === 200 ? (
-                <h1>{me.data.name}</h1>
-            ) : (
-                <h1>Not logged in</h1>
-            )}
-            <form>
-                <label>
-                    Post:
-                    <input
-                        type="text"
-                        name="content"
-                        value={content}
-                        onChange={e => {
-                            setContent(e.target.value);
-                        }}
-                    />
-                </label>
-                <input
-                    type="submit"
-                    value="Post"
-                    onClick={e => {
-                        e.preventDefault();
-                        submit().catch(console.error);
-                    }}
-                />
-            </form>
-            <p className="error">{error}</p>
-            <div className="timeline">
-                {res?.status === 200 ? (
-                    res?.data.map(post => (
-                        <div className="post" key={post.id}>
-                            <p>{post.content}</p>
-                        </div>
-                    ))
-                ) : (
-                    <p>Loading...</p>
-                )}
+  return (
+    <>
+      {me?.status === 200 ? <h1>{me.data.name}</h1> : <h1>Not logged in</h1>}
+      <form>
+        <label>
+          Post:
+          <input
+            type="text"
+            name="content"
+            value={content}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+          />
+        </label>
+        <input
+          type="submit"
+          value="Post"
+          onClick={(e) => {
+            e.preventDefault();
+            submit().catch(console.error);
+          }}
+        />
+      </form>
+      <p className="error">{error}</p>
+      <div className="timeline">
+        {res?.status === 200 ? (
+          res?.data.map((post) => (
+            <div className="post" key={post.id}>
+              <p>{post.content}</p>
             </div>
-        </>
-    );
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Home;

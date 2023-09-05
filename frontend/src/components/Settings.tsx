6a1f7bdb98259bfdb4987532aa0ae8472671b29c
ikanago <ikanago-dev@protonmail.com>
2023-09-05 21:29:46 +0900

    Farewell to prettier and eslint: Welcome, biome!

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -1,71 +1,71 @@
 import { useState } from "react";
-import { updateMe } from "../api";
 import { useNavigate } from "react-router-dom";
+import { updateMe } from "../api";
 
 const Settings = () => {
-    const [displayName, setDisplayName] = useState("");
-    const [summary, setDescription] = useState("");
-    const [avatarUrl, setAvatarUrl] = useState("");
-    const [error, setError] = useState("");
-    const navigate = useNavigate();
+  const [displayName, setDisplayName] = useState("");
+  const [summary, setDescription] = useState("");
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    const submit = async () => {
-        const res = await updateMe({
-            display_name: displayName,
-            summary,
-            avatar_url: avatarUrl,
-        });
-        if (res.status === 204) {
-            navigate("/");
-        } else {
-            setError(res.data.error);
-        }
-    };
+  const submit = async () => {
+    const res = await updateMe({
+      display_name: displayName,
+      summary,
+      avatar_url: avatarUrl,
+    });
+    if (res.status === 204) {
+      navigate("/");
+    } else {
+      setError(res.data.error);
+    }
+  };
 
-    return (
-        <div>
-            <form>
-                <label>
-                    Display Name:
-                    <input
-                        type="text"
-                        name="displayName"
-                        value={displayName}
-                        onChange={e => {
-                            setDisplayName(e.target.value);
-                        }}
-                    />
-                    Description:
-                    <input
-                        type="text"
-                        name="summary"
-                        value={summary}
-                        onChange={e => {
-                            setDescription(e.target.value);
-                        }}
-                    />
-                    Avatar URL:
-                    <input
-                        type="text"
-                        name="avatarUrl"
-                        value={avatarUrl}
-                        onChange={e => {
-                            setAvatarUrl(e.target.value);
-                        }}
-                    />
-                </label>
-                <input
-                    type="submit"
-                    value="Update"
-                    onClick={e => {
-                        e.preventDefault();
-                        submit().catch(console.error);
-                    }}
-                />
-            </form>
-            <p className="error">{error}</p>
-        </div>
-    );
+  return (
+    <div>
+      <form>
+        <label>
+          Display Name:
+          <input
+            type="text"
+            name="displayName"
+            value={displayName}
+            onChange={(e) => {
+              setDisplayName(e.target.value);
+            }}
+          />
+          Description:
+          <input
+            type="text"
+            name="summary"
+            value={summary}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+          Avatar URL:
+          <input
+            type="text"
+            name="avatarUrl"
+            value={avatarUrl}
+            onChange={(e) => {
+              setAvatarUrl(e.target.value);
+            }}
+          />
+        </label>
+        <input
+          type="submit"
+          value="Update"
+          onClick={(e) => {
+            e.preventDefault();
+            submit().catch(console.error);
+          }}
+        />
+      </form>
+      <p className="error">{error}</p>
+    </div>
+  );
 };
 
 export default Settings;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,20 +3,20 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Settings from "./components/Settings";
 import Signup from "./components/Signup";
 import UserProfile from "./components/UserProfile";
-import Settings from "./components/Settings";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/users/:name" element={<UserProfile />} />
-                <Route path="/settings/profile" element={<Settings />} />
-            </Routes>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/users/:name" element={<UserProfile />} />
+        <Route path="/settings/profile" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 );

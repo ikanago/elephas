ac32cd4b23e5605418dd10bb0aef781cbffadbb0
ikanago <ikanago-dev@protonmail.com>
2023-10-01 15:24:09 +0900

    Define RemoteWebfingerRepository

--- backend/src/model/webfinger.rs
+++ backend/src/model/webfinger.rs
@@ -1,3 +1,4 @@
+use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use tracing::info;
 
@@ -14,19 +15,24 @@ pub struct WebfingerLink {
     pub r#type: Option<String>,
 }
 
-pub async fn fetch(user_name: &str, host_name: &str) -> crate::Result<String> {
-    let url = format!(
-        "https://{}/.well-known/webfinger?resource=acct:{}@{}",
-        host_name, user_name, host_name
-    );
-    let webfinger = reqwest::get(url).await?.json::<Webfinger>().await?;;
-    info!(webfinger = ?webfinger.clone());
-    Ok(webfinger
-        .links
-        .iter()
-        .find(|link| link.rel == "self")
-        .ok_or_else(|| crate::error::ServiceError::InternalServerError)?
-        .href
-        .clone()
-        .ok_or_else(|| crate::error::ServiceError::InternalServerError)?)
+#[mockall::automock]
+#[async_trait]
+pub trait RemoteWebfingerRepository {
+    async fn fetch_webfinger(&self, user_name: &str, host_name: &str) -> crate::Result<Webfinger>;
+}
+
+#[derive(Clone)]
+pub struct RemoteWebfingerRepositoryImpl;
+
+#[async_trait]
+impl RemoteWebfingerRepository for RemoteWebfingerRepositoryImpl {
+    async fn fetch_webfinger(&self, user_name: &str, host_name: &str) -> crate::Result<Webfinger> {
+        let url = format!(
+            "https://{}/.well-known/webfinger?resource=acct:{}@{}",
+            host_name, user_name, host_name
+        );
+        let webfinger = reqwest::get(url).await?.json::<Webfinger>().await?;
+        info!(webfinger = ?webfinger.clone());
+        Ok(webfinger)
+    }
 }
--- backend/src/service/remote_user.rs
+++ backend/src/service/remote_user.rs
@@ -2,8 +2,9 @@ use reqwest::{header::ACCEPT, Client};
 use serde::Deserialize;
 
 use super::user_profile::UserProfile;
-use crate::model::webfinger;
+use crate::model::webfinger::{RemoteWebfingerRepository, RemoteWebfingerRepositoryImpl};
 
+#[allow(unused)]
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct ApPerson {
@@ -15,11 +16,21 @@ struct ApPerson {
 }
 
 pub async fn resolve(user_name: &str, host_name: &str) -> crate::Result<UserProfile> {
-    let href = webfinger::fetch(&user_name, &host_name).await?;
-    let client = Client::new();
+    let webfinger = RemoteWebfingerRepositoryImpl
+        .fetch_webfinger(&user_name, &host_name)
+        .await?;
+    let href = webfinger
+        .links
+        .iter()
+        .find(|link| link.rel == "self")
+        .ok_or_else(|| crate::error::ServiceError::InternalServerError)?
+        .href
+        .clone()
+        .ok_or_else(|| crate::error::ServiceError::InternalServerError)?;
+
     let accept =
         r#"application/ld+json; profile="https://www.w3.org/ns/activitystreams"; charset=utf-8"#;
-    let object = client
+    let object = Client::new()
         .get(href)
         .header(ACCEPT, accept)
         .send()
